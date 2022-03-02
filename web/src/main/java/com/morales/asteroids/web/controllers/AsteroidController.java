7812817919a133796f5e8faafa2720155f893392
siwdie <codyjmc@gmail.com>
2022-03-02 20:15:37 +0100

    Changes to swagger descriptions CHANGE: - Added schema example to ApiError - Changes to api responses in AsteroidController

--- web/src/main/java/com/morales/asteroids/web/controllers/AsteroidController.java
+++ web/src/main/java/com/morales/asteroids/web/controllers/AsteroidController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
+import com.morales.asteroids.web.dto.ApiError;
 import com.morales.asteroids.web.dto.Asteroid;
 import com.morales.asteroids.web.services.NasaService;
 
@@ -18,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -38,10 +38,10 @@ public class AsteroidController {
 
     @Operation(summary = "Get asteroids info")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Asteroid data found", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Asteroid.class)))),
-            @ApiResponse(responseCode = "204", description = "No data found"),
-            @ApiResponse(responseCode = "400", description = "Invalid params provided"),
-            @ApiResponse(responseCode = "500", description = "Internal server Error")
+            @ApiResponse(responseCode = "200", description = "Asteroid data found"),
+            @ApiResponse(responseCode = "204", description = "No data found", content = @Content(schema = @Schema(implementation = ApiError.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid params provided", content = @Content(schema = @Schema(implementation = ApiError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server Error", content = @Content(schema = @Schema(implementation = ApiError.class)))
     })
     @GetMapping
     public ResponseEntity<List<Asteroid>> asteroids(@Parameter(description = "days") @RequestParam @Min(value = 1, message = "The number must be between 1 and 7") @Max(value = 7, message = "The number must be between 1 and 7") int days) {
--- web/src/main/java/com/morales/asteroids/web/dto/ApiError.java
+++ web/src/main/java/com/morales/asteroids/web/dto/ApiError.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +15,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ApiError {
+    @Schema(example = "400 BAD REQUEST")
     private HttpStatus status;
+    @Schema(example = "Error with parameter type")
     private String message;
+    @Schema(example = "[\"The parameter \'example\' must be type String\"]")
     private List<String> errors;
 }
